2ebcfb9e9ac473be0117a710368a1961fe5e3804
DMS1997 <dms25121997@gmail.com>
2017-12-29 06:20:21 +0530

    patch dir

--- patches/install.sh
+++ patches/install.sh
@@ -8,7 +8,7 @@ for dir in $dirs ; do
 	cd $rootdirectory
 	cd $dir
 	echo "Applying $dir patches..."
-	git apply $rootdirectory/device/motorola/namath/patches/$dir/*.patch
+	git apply $rootdirectory/$dir/*.patch
 	echo " "
 done
 
